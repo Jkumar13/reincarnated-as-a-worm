bcee00bdb126f186921626fc726e22be9e3593ac
tnguy510 <tnguy510@ucsc.edu>
2024-04-10 18:46:06 -0700

    BG Scroll Skeleton Init

---
+++ CMPM 170 Split 1/Assets/BackgroundScroll.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour {
+    [Range(-1f, 1f)]
+    public float scrollSpeed = 0.5f;
+    private float offset;
+    private Material mat;
+
+    void Start(){
+        mat = GetComponent<Renderer>().material;
+    }
+
+    void Update(){
+        offset += (Time.deltaTime * scrollSpeed) / 10f;
+
+        //EDIT so only scrolls as player is moving
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
