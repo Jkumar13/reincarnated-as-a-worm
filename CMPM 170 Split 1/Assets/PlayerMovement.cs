b53bb3db9de3f9ac8fe305eb6426498c20aa38ed
Jkumar135 <139039476+Jkumar135@users.noreply.github.com>
2024-04-10 17:47:51 -0700

    Mouse control for player movement

--- CMPM 170 Split 1/Assets/PlayerMovement.cs
+++ CMPM 170 Split 1/Assets/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     // Global Variables
     public int playerSpeed;
-    public Vector2 velocity;
+    public Vector2 targetPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,35 +17,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D)) {
-            velocity.x = playerSpeed;
-        }
-        else if (Input.GetKey(KeyCode.A)) {
-            velocity.x = -playerSpeed;
-        }
-        else {
-            velocity.x = 0;
-        }
-        if (Input.GetKey(KeyCode.W)) {
-            velocity.y = playerSpeed;
-        }
-        else if (Input.GetKey(KeyCode.S)) {
-            velocity.y = -playerSpeed;
-        }
-        else {
-            velocity.y = 0;
-        }
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * playerSpeed);
     }
 
-    void FixedUpdate()
-    {
-        Vector2 position = transform.position;
-        
-        position.x += velocity.x * Time.fixedDeltaTime;
-        position.y += velocity.y * Time.fixedDeltaTime;
-        
-        transform.position = position;
-    }
+ 
 
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Enemy")) {
