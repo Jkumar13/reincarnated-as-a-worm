82419dd57f98f4c5ea4a674c6c843aee8f6af64e
Jkumar135 <139039476+Jkumar135@users.noreply.github.com>
2024-04-12 20:56:31 -0700

    jay 2 was here

--- CMPM 170 Split 1/Assets/PlayerMovement.cs
+++ CMPM 170 Split 1/Assets/PlayerMovement.cs
@@ -13,10 +13,9 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
 
-
-        
+        // Ansh was here
+        // Jay2 is here.
     }
 
     // Update is called once per frame
